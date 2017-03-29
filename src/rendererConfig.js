1b4c0c137ff3d772875670db517a2cbe0f99192b
Oscar Johnson <o.johnsson@live.se>
2017-03-29 13:21:16 +0200

    Remove data duplication

--- src/rendererConfig.js
+++ src/rendererConfig.js
@@ -1,12 +1,4 @@
-var pieceColors = {
-	'I': '#27DEFF', //ljusblå
-	'J': '#3C66FF', //blå
-	'L': '#E8740C', //orange
-	'O': '#FFD70D', //gul
-	'S': '#26FF00', //grön
-	'T': '#9E0CE8', //lila
-	'Z': '#FF0000'  //röd
-}
+var pieceColors = require('./colors')
 
 module.exports = {
 	board: {
